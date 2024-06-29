e5160aad369af2c0d44a701cae69c4f8717c7264
Manuel Schinoni <manuelschinoni@hotmail.com>
2024-06-29 03:48:39 -0300

    Updating from another project: Fetchable

--- Unity/Fetchable.cs
+++ Unity/Fetchable.cs
@@ -1,39 +1,44 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Fetchable : MonoBehaviour {
-	new public string name;
-	private static Dictionary<string, Fetchable> dictionary;
+namespace NullrefLib.Unity {
+	public class Fetchable : MonoBehaviour {
+		new public string name;
+		private static Dictionary<string, Fetchable> dictionary;
 
-	private void OnEnable() {
-		if(dictionary == null)
-			dictionary = new Dictionary<string, Fetchable>();
-		if(dictionary.ContainsKey(name))
-			Debug.LogWarning($"There already exists a fetchable item with name {name}. Attempting to fetch this item will yield unreliable results.");
+		private void OnEnable() {
+			dictionary ??= new Dictionary<string, Fetchable>();
 
-		dictionary[name] = this;
-	}
+			if (dictionary.ContainsKey(name))
+				Debug.LogWarning($"There already exists a fetchable item with name {name}. Attempting to fetch this item will yield unreliable results.");
 
-	private void OnDisable() {
-		if(dictionary.ContainsKey(name) && dictionary[name] == this)
-			dictionary.Remove(name);
-	}
+			dictionary[name] = this;
+		}
 
-	public static GameObject Fetch(string name) {
-		if(dictionary.ContainsKey(name))
-			return dictionary[name].gameObject;
-		return null;
-	}
+		private void OnDisable() {
+			if (dictionary.ContainsKey(name) && dictionary[name] == this)
+				dictionary.Remove(name);
+		}
 
-	public static Fetchable FetchRaw(string name) {
-		if(dictionary.ContainsKey(name))
-			return dictionary[name];
-		return null;
-	}
+		public static GameObject Fetch(string name) {
+			if (dictionary.ContainsKey(name))
+				return dictionary[name].gameObject;
 
-	public static T FetchComponent<T>(string name) where T : Component {
-		if(dictionary.ContainsKey(name))
-			return dictionary[name].GetComponent<T>();
-		return null;
+			throw new KeyNotFoundException("Fetchable key not found: " + name);
+		}
+
+		public static Fetchable FetchRaw(string name) {
+			if (dictionary.ContainsKey(name))
+				return dictionary[name];
+
+			throw new KeyNotFoundException("Fetchable key not found: " + name);
+		}
+
+		public static T FetchComponent<T>(string name) where T : Component {
+			if (dictionary.ContainsKey(name))
+				return dictionary[name].GetComponent<T>();
+
+			throw new KeyNotFoundException("Fetchable key not found: " + name);
+		}
 	}
 }
