cb38033caee9f1182cd94e34cc8d730ca1ad21c9
Manuel Schinoni <manuelschinoni@hotmail.com>
2024-06-29 04:13:51 -0300

    Updating from another project: PriorityQueue

--- Core/PriorityQueue.cs
+++ Core/PriorityQueue.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-//using UnityEngine;
 
 namespace NullrefLib.Collections {
 	/// <summary>
@@ -10,46 +9,34 @@ namespace NullrefLib.Collections {
 		private List<(T item, float weight)> queue;
 		private readonly bool greaterFirst;
 
-		public int Count {
-			get {
-				return queue.Count;
-			}
-		}
+		public int Count => queue.Count;
 
 		public PriorityQueue(bool priorityIsGreater = true) {
 			queue = new List<(T item, float weight)>();
 			greaterFirst = priorityIsGreater;
 		}
 
-		public void Enqueue(T item, float cost) {
-			queue.Add((item, cost));
-		}
+		public void Enqueue(T item, float cost) => queue.Add((item, cost));
 
-		public void Enqueue((T item, float weight) item) {
-			queue.Add(item);
-		}
+		public void Enqueue((T item, float weight) item) => queue.Add(item);
 
-		public T Dequeue() {
-			if(greaterFirst)
-				return SearchGreater().item;
-			else
-				return SearchLesser().item;
-		}
+		public T Dequeue() => greaterFirst ? SearchGreater().item : SearchLesser().item;
 
 		private (T item, float weight) SearchLesser() {
 			float testValue = float.PositiveInfinity;
 			int testIndex = -1;
 			int queueCount = queue.Count;
-			for(int i = 0; i < queueCount; i++)
+			for (int i = 0; i < queueCount; i++)
 			{
-				if(queue[i].weight <= testValue)
+				if (queue[i].weight <= testValue)
 				{
 					testIndex = i;
 					testValue = queue[i].weight;
 				}
 			}
-			if(testIndex == -1)
+			if (testIndex == -1)
 				return default;
+
 			(T item, float weight) returned = queue[testIndex];
 			queue.RemoveAt(testIndex);
 			return returned;
@@ -60,34 +47,35 @@ namespace NullrefLib.Collections {
 			float testValue = float.NegativeInfinity;
 			int testIndex = -1;
 			int queueCount = queue.Count;
-			for(int i = 0; i < queueCount; i++)
+			for (int i = 0; i < queueCount; i++)
 			{
-				if(queue[i].weight >= testValue)
+				if (queue[i].weight >= testValue)
 				{
 					testIndex = i;
 					testValue = queue[i].weight;
 				}
 			}
-			if(testIndex == -1)
+			if (testIndex == -1)
 				return default;
+
 			(T item, float weight) returned = queue[testIndex];
 			queue.RemoveAt(testIndex);
 			return returned;
 
 		}
 
-		public void Clear() =>
+		public void Clear() {
 			queue.Clear();
+		}
 
 		public bool Contains(T item) {
 			int c = queue.Count;
-			for(int i = 0; i < c; i++)
+			for (int i = 0; i < c; i++)
 			{
-				if(queue[i].item.Equals(item))
+				if (queue[i].item.Equals(item))
 					return true;
 			}
 			return false;
 		}
 	}
-
 }
