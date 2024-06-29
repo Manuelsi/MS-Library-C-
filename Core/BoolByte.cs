8f15d15f91bd05b3da80475db25b09bb110b5cb9
Manuel Schinoni <manuelschinoni@hotmail.com>
2024-06-29 04:06:44 -0300

    Updating from another project: Boolbyte

--- Core/BoolByte.cs
+++ Core/BoolByte.cs
@@ -5,25 +5,35 @@ namespace NullrefLib {
 	/// Structure containing 8 boolean values stored inside a byte.
 	/// Intended to optimize the use of boolean values in memory at the cost of processing.
 	/// </summary>
+	[Serializable]
 	public struct BoolByte {
+
+#if UNITY_EDITOR || UNITY_STANDALONE
+		[UnityEngine.SerializeField]
+#endif
 		private byte data;
 
 		public byte RawData => data;
+
+		public bool AnyTrue => data != 0;
 
 		/// <summary>
 		/// Create new BoolByte package from boolean values.
 		/// </summary>
 		/// <param name="data">Boolean values.</param>
 		public BoolByte(params bool[] data) {
-			int length;
-			if(data.Length > 8)
+			if (data == null)
 			{
-				length = 8;
+				this.data = 0;
+				return;
+			}
+
+			int length = data.Length;
+			if (length > 8)
 				throw new ArgumentException("BoolByte created was provided with more than 8 values. Values with index 8 and above ignored.");
-			} else
-				length = data.Length;
+
 			this.data = 0;
-			for(int i = 0; i < length; i++)
+			for (int i = 0; i < length; i++)
 			{
 				this.data = this.data.SetBit(i, data[i]);
 			}
@@ -33,31 +43,29 @@ namespace NullrefLib {
 		/// Create new BoolByte package from a preexisting byte.
 		/// </summary>
 		/// <param name="data">Byte value.</param>
-		public BoolByte(byte data) {
-			this.data = data;
-		}
+		public BoolByte(byte data) => this.data = data;
 
 		public bool this[int i] {
 			get {
-				if(i < 0 || i > 7)
+				if (i < 0 || i > 7)
 					throw new System.IndexOutOfRangeException();
+
 				return data.GetBit(i);
 			}
 			set {
-				if(i < 0 || i > 7)
+				if (i < 0 || i > 7)
 					throw new System.IndexOutOfRangeException();
+
 				data = data.SetBit(i, value);
 			}
 		}
 
 		#region Overrides
-		public override bool Equals(object obj) {
-			return (data.Equals(obj) && (obj.GetType() == typeof(BoolByte)));
-		}
+		public override bool Equals(object obj) => 
+			data.Equals(obj) && (obj.GetType() == typeof(BoolByte));
 
-		public override int GetHashCode() {
-			return (((data.GetHashCode() + 14388) * 3) - 2155) / 4;
-		}
+		public override int GetHashCode() =>
+			(((data.GetHashCode() + 14388) * 3) - 2155) / 4;
 		#endregion
 
 		#region Operators
