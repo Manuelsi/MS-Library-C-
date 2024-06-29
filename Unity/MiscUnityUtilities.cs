ad340dc1fbebe2bc5a99fd70f721b3cc8e19a586
Manuel Schinoni <manuelschinoni@hotmail.com>
2024-06-29 03:13:13 -0300

    Updating from another project: MiscUnityUtilities

--- Unity/MiscUnityUtilities.cs
+++ Unity/MiscUnityUtilities.cs
@@ -1,168 +1,11 @@
 ﻿using System;
 using System.Globalization;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 // compile with: -doc:DocFileName.xml 
 namespace NullrefLib.Unity {
 
 	public static class MiscUnityUtilities {
-		#region Detectors
-		#region 2D Detector
-		/// <summary>
-		/// Compact implementation of a "Line of Sight"/"Detection" algorithm in 2D.
-		/// </summary>
-		/// <param name="transform">Transform of the detector entity</param>
-		/// <param name="detectionLayerMask">Layer mask of detectable targets</param>
-		/// <param name="detectionRange">Detection radius from detector's pivot/centre</param>
-		/// <param name="detectionFOV">Detection Fielf of View in degrees</param>
-		/// <param name="detectionSightlineLayerMask">"Opaque" objects layer mask. Detector will ignore those excluded layers when determining visibility to target.
-		/// Target must be included</param>
-		/// <param name="target">First acquired target. Null if none found.</param>
-		/// <param name="usesRight">When determining field of view, assign to 'true' if 'transform.right' is considered the front. Otherwise it will use 'transform.forward'.</param>
-		/// <returns></returns>
-		public static bool Detection2D(Transform transform, LayerMask detectionLayerMask, float detectionRange,
-			float detectionFOV, LayerMask detectionSightlineLayerMask, out Transform target, bool usesRight = false) {
-
-			Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, detectionRange, detectionLayerMask);
-			if (cols != null && cols.Length > 0)
-			{
-				foreach (var item in cols)
-				{
-					Vector2 relativePos = item.transform.position - transform.position;
-					if (Vector2.Angle((usesRight ? transform.right : transform.forward), relativePos) < detectionFOV * 0.5f)
-					{
-						RaycastHit2D hit = Physics2D.Raycast(transform.position, relativePos, detectionRange,
-							detectionSightlineLayerMask);
-						if (detectionLayerMask.ContainsLayer(hit.transform.gameObject.layer))
-						{
-							target = hit.transform;
-							return true;
-						}
-					}
-				}
-			}
-			target = null;
-			return false;
-		}
-		#endregion
-		#region 3D Detector
-		/// <summary>
-		/// Compact implementation of a "Line of Sight"/"Detection" algorithm in 3D.
-		/// </summary>
-		/// <param name="transform">Transform of the detector entity</param>
-		/// <param name="detectionLayerMask">Layer mask of detectable targets</param>
-		/// <param name="detectionRange">Detection radius from detector's pivot/centre</param>
-		/// <param name="detectionFOV">Detection Fielf of View in degrees</param>
-		/// <param name="detectionSightlineLayerMask">"Opaque" objects layer mask. Detector will ignore those excluded layers when determining visibility to target.
-		/// Target must be included</param>
-		/// <param name="target">First acquired target. Null if none found.</param>
-		/// <param name="usesRight">When determining field of view, assign to 'true' if 'transform.right' is considered the front. Otherwise it will use 'transform.forward'.</param>
-		/// <returns></returns>
-		public static bool Detection3D(Transform transform, LayerMask detectionLayerMask, float detectionRange,
-			float detectionFOV, LayerMask detectionSightlineLayerMask, out Transform target, bool usesRight = false) =>
-			Detection3D(transform, detectionLayerMask, detectionRange, detectionFOV,
-				detectionSightlineLayerMask, out target, Vector3.zero, usesRight);
-		
-		/// <summary>
-		/// Compact implementation of a "Line of Sight"/"Detection" algorithm in 3D.
-		/// </summary>
-		/// <param name="transform">Transform of the detector entity</param>
-		/// <param name="detectionLayerMask">Layer mask of detectable targets</param>
-		/// <param name="detectionRange">Detection radius from detector's pivot/centre</param>
-		/// <param name="detectionFOV">Detection Fielf of View in degrees</param>
-		/// <param name="detectionSightlineLayerMask">"Opaque" objects layer mask. Detector will ignore those excluded layers when determining visibility to target.
-		/// Target must be included</param>
-		/// <param name="target">First acquired target. Null if none found.</param>
-		/// <param name="deltaPos">View offset for when the "eyes" are not located at the object pivot</param>
-		/// <param name="usesRight">When determining field of view, assign to 'true' if 'transform.right' is considered the front. Otherwise it will use 'transform.forward'.</param>
-		/// <returns></returns>
-		public static bool Detection3D(Transform transform, LayerMask detectionLayerMask, float detectionRange,
-			float detectionFOV, LayerMask detectionSightlineLayerMask, out Transform target, Vector3 deltaPos, bool usesRight = false) =>
-			 Detection3D(transform, detectionLayerMask, detectionRange, detectionFOV,
-				 detectionSightlineLayerMask, out target, deltaPos, usesRight ? Vector3.right : Vector3.forward);
-		
-
-		/// <summary>
-		/// Compact implementation of a "Line of Sight"/"Detection" algorithm in 3D.
-		/// </summary>
-		/// <param name="transform">Transform of the detector entity</param>
-		/// <param name="detectionLayerMask">Layer mask of detectable targets</param>
-		/// <param name="detectionRange">Detection radius from detector's pivot/centre</param>
-		/// <param name="detectionFOV">Detection Fielf of View in degrees</param>
-		/// <param name="detectionSightlineLayerMask">"Opaque" objects layer mask. Detector will ignore those excluded layers when determining visibility to target.
-		/// Target must be included</param>
-		/// <param name="target">First acquired target. Null if none found.</param>
-		/// <param name="deltaPos">View offset for when the "eyes" are not located at the object pivot</param>
-		/// <param name="frontDirection">Determines the direction considered as "front", relative to the transform.
-		/// It will be rotated accordingly to the transform's rotation.</param>
-		/// <returns></returns>
-		public static bool Detection3D(Transform transform, LayerMask detectionLayerMask, float detectionRange,
-			float detectionFOV, LayerMask detectionSightlineLayerMask, out Transform target, Vector3 deltaPos, Vector3 frontDirection) {
-			Vector3 refPos = transform.position + (transform.rotation * deltaPos);
-			Collider[] cols = Physics.OverlapSphere(refPos, detectionRange, detectionLayerMask);
-			if(cols != null && cols.Length > 0)
-			{
-				foreach(var item in cols)
-				{
-					Vector3 relativePos = item.transform.position - refPos;
-					if(Vector3.Angle((transform.rotation * frontDirection), relativePos) < detectionFOV * 0.5f)
-					{
-						RaycastHit hit;
-						bool didHit = Physics.Raycast(refPos, relativePos, out hit, detectionRange,
-							detectionSightlineLayerMask);
-						if(didHit && detectionLayerMask.ContainsLayer(hit.transform.gameObject.layer))
-						{
-							target = hit.transform;
-							return true;
-						}
-					}
-				}
-			}
-			target = null;
-			return false;
-		}
-		#endregion
-		#region 3D Detector All
-		public static bool Detection3DAll(Transform transform, LayerMask detectionLayerMask, float detectionRange,
-			float detectionFOV, LayerMask detectionSightlineLayerMask, out List<Transform> targets, Vector3 deltaPos, bool usesRight = false) {
-
-			Vector3 refPos = transform.position + (transform.rotation * deltaPos);
-			Collider[] cols = Physics.OverlapSphere(refPos, detectionRange, detectionLayerMask);
-			bool found = false;
-			targets = new List<Transform>();
-
-			if (cols != null && cols.Length > 0)
-			{
-				foreach (var item in cols)
-				{
-					Vector3 relativePos = item.transform.position - transform.position;
-					if (Vector3.Angle((usesRight ? transform.right : transform.forward), relativePos) < detectionFOV * 0.5f)
-					{
-						RaycastHit hit;
-
-						bool didHit = Physics.Raycast(refPos, relativePos, out hit, detectionRange,
-							detectionSightlineLayerMask);
-						if (didHit && detectionLayerMask.ContainsLayer(hit.transform.gameObject.layer))
-						{
-							targets.Add(hit.transform);
-							found = true;
-						}
-					}
-				}
-			}
-			return found;
-		}
-
-		public static bool Detection3DAll(Transform transform, LayerMask detectionLayerMask, float detectionRange,
-			float detectionFOV, LayerMask detectionSightlineLayerMask, out List<Transform> targets, bool usesRight = false) {
-
-			return Detection3DAll(transform, detectionLayerMask, detectionRange, detectionFOV, detectionSightlineLayerMask, out targets, Vector3.zero, usesRight);
-		}
-		#endregion
-		#endregion
-
 		public static Quaternion Slerp2D(Vector2 a, Vector2 b, float t, bool usesRight = false) {
 
 			float fa;
@@ -179,13 +22,13 @@ namespace NullrefLib.Unity {
 			}
 
 			Quaternion qa = Quaternion.AngleAxis(fa, Vector3.forward);
-			Quaternion qb = Quaternion.AngleAxis(fb, Vector3.forward);
+			Quaternion qb = Quaternion.AngleAxis(fb, Vector3.kforward);
 
 			return Quaternion.Slerp(qa, qb, t);
 		}
 
 		public static Quaternion SlerpWithAxises(Vector3 a, Vector3 b, float t, Vector3 reference, Vector3 axis) {
-			
+
 			float fa = Vector3.SignedAngle(reference, a, axis);
 			float fb = Vector3.SignedAngle(reference, b, axis);
 
@@ -220,7 +63,7 @@ namespace NullrefLib.Unity {
 		/// <param name="layer">Layer to create a layer mask from (0-31)</param>
 		/// <returns></returns>
 		public static LayerMask IntToLayerMask(int layer) {
-			return (1 << layer);
+			return 1 << layer;
 		}
 
 		/// <summary>
@@ -230,11 +73,11 @@ namespace NullrefLib.Unity {
 		/// <returns></returns>
 		public static Color ParseColor(string hex) {
 			int length = hex.Length;
-			if(!(length == 6 || length == 8))
+			if (!(length == 6 || length == 8))
 				throw new ArgumentException($"Color Hex code {hex} is not a valid hex code.");
 
-			var color = new Color32();
-			if(
+			Color32 color = new();
+			if (
 				byte.TryParse(hex.Substring(0, 2), NumberStyles.AllowHexSpecifier, NumberFormatInfo.InvariantInfo, out byte r) &&
 				byte.TryParse(hex.Substring(2, 2), NumberStyles.AllowHexSpecifier, NumberFormatInfo.InvariantInfo, out byte g) &&
 				byte.TryParse(hex.Substring(4, 2), NumberStyles.AllowHexSpecifier, NumberFormatInfo.InvariantInfo, out byte b))
@@ -245,21 +88,22 @@ namespace NullrefLib.Unity {
 			} else
 				throw new ArgumentException($"Color Hex code {hex} is not a valid hex code.");
 
-			if(length == 8)
-				if(byte.TryParse(hex.Substring(6, 2), NumberStyles.AllowHexSpecifier, NumberFormatInfo.InvariantInfo, out byte a))
+
+			if (length == 8)
+			{
+				if (byte.TryParse(hex.Substring(6, 2), NumberStyles.AllowHexSpecifier, NumberFormatInfo.InvariantInfo, out byte a))
 					color.a = a;
 				else
 					throw new ArgumentException($"Color Hex code {hex} is not a valid hex code.");
-			else
+
+			} else
 				color.a = 0xFF;
 
 			return color;
 		}
 
 		public static void Swap(ref float v1, ref float v2) {
-			float temp = v1;
-			v1 = v2;
-			v2 = temp;
+			(v2, v1) = (v1, v2);
 		}
 	}
 }
